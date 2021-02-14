472916b2344c7e54dba0e4b663bb94489f8fbc5c
BCarlson1512 <benproskilled@gmail.com>
2021-02-13 19:09:57 -0500

    small performance changes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Navbar({open, setOpen}) {
     return (
         <div>
-            <div className={open ? "viewport-open" : "viewport-nav"} style={{fontFamily: 'Syncopate, sans-serif' , fontSize: '4vh' , letterSpacing:'0.25vw'}} onClick={() => setOpen(!open)}>
+            <div className={open ? "viewport-nav" : "viewport-open"} style={{fontFamily: 'Syncopate, sans-serif' , fontSize: '4vh' , letterSpacing:'0.25vw'}} onClick={() => setOpen(!open)}>
                 <Link to="/">My Tasks</Link>
                 <Link to="/create">Create Tasks</Link>
                 <Link to="/login">Login</Link>
