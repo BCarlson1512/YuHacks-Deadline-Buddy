ef52e1c7421f58a7185a382e6d84f6797ea20f1e
JP Sanchez <Mark_JP.Sanchez@yahoo.ca>
2021-02-14 01:37:35 -0500

    Started switch to static files

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import path from 'path'
 import userRouter from './Routers/UserRouter.js';
 import taskRouter from './Routers/TaskRouter.js';
 
 const app = express();
+
+app.use(express.static(path.join(path.resolve(), '../frontend/build')));
+
 app.use(express.json()); // parse http requests
 app.use(express.urlencoded({ extended: true }));
 
@@ -16,12 +20,12 @@ mongoose.connect('mongodb+srv://ben:Pr0ski113f2!@cluster0.nzwg9.mongodb.net/test
 app.use('/api/users', userRouter);
 app.use('/api/tasks', taskRouter);
 
-app.get("/", (req, res) => {
-    res.send('Server is Ready');
+app.get("*", (req, res) => {
+    res.sendFile(path.join(path.resolve(), '../frontend/build/index.html'));
 })
 
 app.use((err, req, res, next) => {
-    res.status(500).send("Server error");
+    res.status(500).send("Server error" + err);
 })
 
 const port = 5000;
